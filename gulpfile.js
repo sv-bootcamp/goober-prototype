ae93cdceb107d4b9dc3a6b5e1c0048e824882718
festinalente91 <nasuk0610@gmail.com>
2016-08-17 22:25:41 +0900

    Add auto restart gulp task
    - using child-process

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
 var gulp = require('gulp');
+var spawn = require('child_process').spawn;
 var babel = require('gulp-babel');
 
-var serverJSFiles = ['./server.js', './src/server/**/*.js'];
+var node;
 
-gulp.task('default', function(){
+var serverJSFiles = ['./server.js', './src/server/**/*.js'];
+var serverTasks = ['babel', 'server'];
+var clientTasks = [];
+
+gulp.task('default', serverTasks , function() {
+
+  gulp.watch(serverJSFiles, serverTasks);
+  
+});
+
+
+gulp.task('server', ['babel'], function() {
+  
+  if (node) node.kill();
+  
+  node = spawn('node', ['./dist-server/server.js'], {stdio: 'inherit'})
+  
+  node.on('close', function (code) {
+    if (code === 8) {
+      gulp.log('Error detected, waiting for changes...');
+    }
+  });
 
 });
 
-gulp.task('babel', () =>
-    gulp.src(serverJSFiles)
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist-server'))
-);
+
+// clean up if an error goes unhandled.
+process.on('exit', function() {
+    if (node) node.kill()
+})
+
+gulp.task('babel', () =>{
+  return gulp.src(serverJSFiles)
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist-server'))
+});
